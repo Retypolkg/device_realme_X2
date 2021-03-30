992641d75c4d53c5e3c2c7bbdb28d71fe7cf727b
Retypolkg <damian11vp.pl@gmail.com>
2021-03-30 15:05:36 +0000

    X2: Derpfest-ify

--- derp_X2.mk
+++ derp_X2.mk
@@ -6,11 +6,17 @@
 
 $(call inherit-product, device/realme/X2/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Derpfest stuff.
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+TARGET_USES_BLUR := true
+EXTRA_FOD_ANIMATIONS := true
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_X2
+PRODUCT_NAME := derp_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
@@ -20,9 +26,4 @@ BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/relea
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
-# Nezuko OS Properties
-TARGET_BOOT_ANIMATION_RES := 1080
-CUSTOM_BUILD_TYPE := OFFICIAL
-NEZUKO_MAINTAINER := Retypolkg
-PRODUCT_PRODUCT_PROPERTIES += \
-  ro.nezuko.cpu=SD730G
+
